bff99c1c8c56cc34e2ac6a5bcd4d22fc539cd6d6
duykasama <duykasama@gmail.com>
2024-02-24 12:15:16 +0700

    [Duy] Load Level_1 from StartMenu

---
+++ Assets/Scenes/StartMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OpenSettingsMenu()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
